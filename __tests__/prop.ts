88b4bccbace6afbd56d6e7476af7715f4569ebe7
Elcoosp <elcoosp@gmail.com>
2018-05-20 22:57:27 +0200

    test(prop): Add test

---
+++ __tests__/prop.ts
@@ -0,0 +1,9 @@
+const { prop } = require('../src/prop.ts')
+
+it('should return true if the property equal the value on the last argument object', () => {
+  expect(prop('id').eq(3)({ id: 3 })).toBe(true)
+})
+
+it('should return false if the property is not equal the value on the last argument object', () => {
+  expect(prop('id').eq(2)({ id: 3 })).toBe(false)
+})
