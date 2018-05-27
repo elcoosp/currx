9f652e58d2a1fb6cc4475c94620e814c7c556150
Elcoosp <elcoosp@gmail.com>
2018-05-27 22:03:36 +0200

    fix(prop): Add ObjToLookUp interface for bracket notation access

--- src/prop.ts
+++ src/prop.ts
@@ -1,9 +1,9 @@
+import { ObjToLookUp } from './types'
+
 export interface PropObject {
   eq: (data: any) => (o: ObjToLookUp) => boolean
 }
-export interface ObjToLookUp {
-  [x: string]: any
-}
+
 export const prop = (prop: string): PropObject => ({
   eq: data => o => o[prop] === data
 })
